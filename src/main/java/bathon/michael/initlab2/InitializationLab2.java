e856152efe66c965bb8192bc548be5421d552067
Michael Bathon <michaelbathon@Zips-MacBook-Pro-2.local>
2016-01-28 15:04:33 -0500

    Completed Part1 of InitLab2

--- src/main/java/bathon/michael/initlab2/InitializationLab2.java
+++ src/main/java/bathon/michael/initlab2/InitializationLab2.java
@@ -5,5 +5,77 @@ package bathon.michael.initlab2;
  */
 public class InitializationLab2 {
 
+    public static void main(String[] args) {
+        ThingContainer container1 = new ThingContainer(3);//(int)(Math.random()*4) + 3);
+        ThingContainer container2 = new ThingContainer(4);//(int)(Math.random()*5) + 4);
+        ThingContainer container3 = new ThingContainer(5);//(int)(Math.random()*6) + 5);
+        ThingContainer container4 = new ThingContainer(4);
+
+        container1.add(new ColorfulThing(ColorEnum.BLUE));
+        container1.add(new ColorfulThing(ColorEnum.RED));
+        container1.add(new ColorfulThing(ColorEnum.GREEN));
+        container1.add(new ColorfulThing(ColorEnum.BLUE));
+
+        container2.add(new ColorfulThing(ColorEnum.GREEN));
+        container2.add(new ColorfulThing(ColorEnum.RED));
+
+
+        container4.add(new ColorfulThing(ColorEnum.BLUE));
+        container4.add(new ColorfulThing(ColorEnum.RED));
+        container4.add(new ColorfulThing(ColorEnum.GREEN));
+
+        container1.printThings();
+        container2.printThings();
+        container3.printThings();
+
+
+    }
+
+}
+
+enum ColorEnum {RED, BLUE, GREEN}
+
+class ColorfulThing {
+
+    private ColorEnum colorEnum;
+
+    ColorfulThing(ColorEnum colorEnum){
+        this.colorEnum = colorEnum;
+    }
+
+
+    public ColorEnum getColor(){
+        return this.colorEnum;
+    }
+}
+
+class ThingContainer {
+    ColorfulThing[] colorArray;
+    int i = 0;
+
+    public ThingContainer(int arraySize) {
+        colorArray = new ColorfulThing[arraySize];
+    }
+
+    public int getArray() {
+        return colorArray.length;
+    }
+
+    public void add(ColorfulThing colorObj) {
+        if (i >= colorArray.length) {
+            System.out.println("ThingContainer is full");
+            return;}
+        colorArray[i] = colorObj;
+        i++;
+    }
+
+    public void printThings(){
+        for(int x = 0; x < colorArray.length; x++){
+            if(colorArray[x] == null){
+                return;
+            }else
+                System.out.println(colorArray[x].getColor());
+        }
+    }
 
 }
--- src/test/java/bathon/michael/initlab2/SpecInitializationLab2.java
+++ src/test/java/bathon/michael/initlab2/SpecInitializationLab2.java
@@ -1,9 +1,53 @@
 package bathon.michael.initlab2;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by michaelbathon on 1/28/16.
  */
 public class SpecInitializationLab2 {
 
+    ThingContainer container;
+
+
+    @Before
+    public void setUp(){
+
+        container = new ThingContainer(5);
+
+        container.add(new ColorfulThing(ColorEnum.BLUE));
+        container.add(new ColorfulThing(ColorEnum.RED));
+        container.add(new ColorfulThing(ColorEnum.GREEN));
+        container.add(new ColorfulThing(ColorEnum.RED));
+        container.add(new ColorfulThing(ColorEnum.GREEN));
+
+
+
+    }
+
+    @Test
+    public void testContainerArraySizeConstructor(){
+       int x = 5;
+
+        assertEquals("checking the array is constructed and of the same size", container.getArray(), x );
+    }
+
+
+   @Test
+    public void testArrayAddMethod(){
+
+        assertNotNull(container.colorArray[3]);
+
+    }
+
+    @Test
+    public void testPrinter(){
+        container.printThings();
+    }
 
 }
