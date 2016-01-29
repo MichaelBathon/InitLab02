130e165d306a4da3a5ae9c2d1635c8d40f0f0207
Michael Bathon <michaelbathon@Zips-MacBook-Pro-2.local>
2016-01-28 22:29:09 -0500

    Completed InitLab2 Part2 and Test

--- src/main/java/bathon/michael/initlab2/InitializationLab2.java
+++ src/main/java/bathon/michael/initlab2/InitializationLab2.java
@@ -1,5 +1,7 @@
 package bathon.michael.initlab2;
 
+import java.util.Arrays;
+
 /**
  * Created by michaelbathon on 1/28/16.
  */
@@ -9,7 +11,11 @@ public class InitializationLab2 {
         ThingContainer container1 = new ThingContainer(3);//(int)(Math.random()*4) + 3);
         ThingContainer container2 = new ThingContainer(4);//(int)(Math.random()*5) + 4);
         ThingContainer container3 = new ThingContainer(5);//(int)(Math.random()*6) + 5);
-        ThingContainer container4 = new ThingContainer(4);
+        ThingContainer container4 = new ThingContainer(3);
+
+        ColorfulThing[] hue = new ColorfulThing[4];
+        ThingContainer container5 = new ThingContainer(hue);
+
 
         container1.add(new ColorfulThing(ColorEnum.BLUE));
         container1.add(new ColorfulThing(ColorEnum.RED));
@@ -19,21 +25,40 @@ public class InitializationLab2 {
         container2.add(new ColorfulThing(ColorEnum.GREEN));
         container2.add(new ColorfulThing(ColorEnum.RED));
 
+        ColorfulThing color1 = new ColorfulThing(ColorEnum.RED);
+        ColorfulThing color2 = new ColorfulThing(ColorEnum.GREEN);
+        ColorfulThing color3 = new ColorfulThing(ColorEnum.BLUE);
 
-        container4.add(new ColorfulThing(ColorEnum.BLUE));
-        container4.add(new ColorfulThing(ColorEnum.RED));
-        container4.add(new ColorfulThing(ColorEnum.GREEN));
+        ThingContainer container6 = new ThingContainer(container2.colorArray);
+
+
+        container4.add(color1);
+        container4.add(color2);
+        container4.add(color3);
 
         container1.printThings();
         container2.printThings();
         container3.printThings();
+
+        //System.out.println(container1.pop().getColor());
+        //System.out.println(container2.pop().getColor());
+        //System.out.println(container4.pop().getColor());
+        //System.out.println(container4.pop().getColor());
+
+        container1.remove(ColorEnum.RED);
+        System.out.println(container4.remove(color2).getColor());
+        System.out.println(Arrays.toString(container4.colorArray));
+        System.out.println(container4.colorArray[1].getColor());
+        System.out.println(container5.pigmentArray.length);
+        System.out.println(container6.pigmentArray[1].getColor());
+
 
 
     }
 
 }
 
-enum ColorEnum {RED, BLUE, GREEN}
+enum ColorEnum {RED, BLUE, GREEN, ORANGE}
 
 class ColorfulThing {
 
@@ -51,10 +76,17 @@ class ColorfulThing {
 
 class ThingContainer {
     ColorfulThing[] colorArray;
-    int i = 0;
+    int ctCount = 0;
+    ColorfulThing[] pigmentArray;
+
+
 
     public ThingContainer(int arraySize) {
         colorArray = new ColorfulThing[arraySize];
+    }
+
+    public ThingContainer(ColorfulThing[] x){
+        pigmentArray = x;
     }
 
     public int getArray() {
@@ -62,11 +94,11 @@ class ThingContainer {
     }
 
     public void add(ColorfulThing colorObj) {
-        if (i >= colorArray.length) {
+        if (ctCount >= colorArray.length) {
             System.out.println("ThingContainer is full");
             return;}
-        colorArray[i] = colorObj;
-        i++;
+        colorArray[ctCount] = colorObj;
+        ctCount++;
     }
 
     public void printThings(){
@@ -77,5 +109,57 @@ class ThingContainer {
                 System.out.println(colorArray[x].getColor());
         }
     }
+
+    public ColorfulThing pop(){
+        if(colorArray[0] == null){
+            return null;
+        }
+        ColorfulThing poppedObj = colorArray[ctCount -1];
+        colorArray[ctCount -1] = null;
+        ctCount--;
+        return poppedObj;
+    }
+
+    public ColorfulThing remove(ColorEnum colorEnum ){
+        int z;
+        ColorfulThing enumSearchVal = null;
+
+        for(z = 0; z < ctCount; z++){
+            if (colorEnum == colorArray[z].getColor()){
+                System.out.println("Found the color.");
+                enumSearchVal = colorArray[z];
+                colorArray[z] = null;
+                break;
+            }
+        }
+        while(z < ctCount-1){
+            colorArray[z] = colorArray[z+1];
+            z++;
+        }
+        colorArray[ctCount -1] = null;
+        ctCount--;
+        return enumSearchVal;
+    }
+
+    public ColorfulThing remove(ColorfulThing obj){
+        int b;
+        ColorfulThing removedColorObj = null;
+
+        for(b = 0; b < ctCount; b++) {
+            if (obj.getColor() == colorArray[b].getColor()) {
+                removedColorObj = colorArray[b];
+                colorArray[b] = null;
+                break;
+            }
+        }
+        while(b < ctCount-1){
+            colorArray[b] = colorArray[b+1];
+            b++;
+        }
+        colorArray[ctCount -1] = null;
+        ctCount--;
+    return removedColorObj;
+    }
+
 
 }
--- src/test/java/bathon/michael/initlab2/SpecInitializationLab2.java
+++ src/test/java/bathon/michael/initlab2/SpecInitializationLab2.java
@@ -13,19 +13,21 @@ import static org.junit.Assert.*;
 public class SpecInitializationLab2 {
 
     ThingContainer container;
+    ThingContainer container2;
 
 
     @Before
     public void setUp(){
 
         container = new ThingContainer(5);
+        container2 = new ThingContainer(6);
+
 
         container.add(new ColorfulThing(ColorEnum.BLUE));
         container.add(new ColorfulThing(ColorEnum.RED));
         container.add(new ColorfulThing(ColorEnum.GREEN));
         container.add(new ColorfulThing(ColorEnum.RED));
         container.add(new ColorfulThing(ColorEnum.GREEN));
-
 
 
     }
@@ -48,6 +50,62 @@ public class SpecInitializationLab2 {
     @Test
     public void testPrinter(){
         container.printThings();
+    }
+
+    @Test
+    public void testPop(){
+        assertEquals("Fails if it doesn't return a ColorfulThing object removed from the last position in the" +
+                " array", container.colorArray[4], container.pop());
+    }
+
+    @Test
+    public void testEnumRemove(){
+
+     /*   assertEquals("Fails if expected object doesn't have the same value as the removed Enum" +
+                " searched ColorfulThing obj", ColorEnum.GREEN, container.remove(ColorEnum.GREEN).getColor()); */
+
+        ColorfulThing removedThing = container.remove(ColorEnum.RED);
+
+        assertNotNull(removedThing);
+
+        ColorEnum result = removedThing.getColor();
+
+        assertEquals("Fails if expected object doesn't have the same value as the removed Enum" +
+                " searched ColorfulThing obj", ColorEnum.RED, result);
+
+        System.out.println(result);
+
+        assertNull("Fails if there is an object in the last index of the array", container.colorArray[container.colorArray.length -1]);
+    }
+
+    @Test
+    public void testNullRemove(){
+        assertEquals("Fails if it doesn't get null", null, container.remove(ColorEnum.ORANGE));
+    }
+
+    @Test
+    public void testColorfulThingObjRemove(){
+        ColorfulThing ctRed = new ColorfulThing(ColorEnum.RED);
+        ColorfulThing ctBlue = new ColorfulThing(ColorEnum.BLUE);
+        ColorfulThing ctGreen = new ColorfulThing(ColorEnum.GREEN);
+        ColorfulThing ctRed2 = new ColorfulThing(ColorEnum.RED);
+
+        container2.add(ctRed);
+        container2.add(ctBlue);
+        container2.add(ctGreen);
+        container2.add(ctRed2);
+
+        ColorfulThing removedObjThing = container2.remove(ctGreen);
+
+        assertNotNull(removedObjThing);
+
+        assertEquals("Fails if expected object doesn't have the same value as the removed Enum" +
+                " searched ColorfulThing obj", ColorEnum.GREEN, removedObjThing.getColor());
+
+        System.out.println(container2.colorArray[2].getColor());
+
+        assertNull("Fails if there is an object in the last index of the array", container2.colorArray[container2.colorArray.length -1]);
+
     }
 
 }
